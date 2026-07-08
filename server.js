c510a0cc54471ec3eb1d680997c33580f9f05234
Nitin <rajnitin793@gmail.com>
2026-07-08 20:28:03 +0530

    setup mogo and connected it to project

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const app = require('./src/app')
+const connectDB = require("./src/db/db")
 
 const PORT = parseInt(process.env.PORT) || 3000
+
+connectDB()
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })  

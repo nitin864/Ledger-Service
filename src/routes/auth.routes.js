5858caaacb0289c77280b3f391fc4e3b12b352ef
Nitin <rajnitin793@gmail.com>
2026-07-17 21:02:17 +0530

    connected loginController with respective routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,11 @@
 const express  = require('express')
 const authControllers = require('../controllers/auth.controller')
+const loginUserController = require('../controllers/auth.controller')
 const router = express.Router()
 
 
+
 router.post('/register', authControllers.registerUserController)
+router.post('/login', authControllers.loginUserController)
 
 module.exports = router

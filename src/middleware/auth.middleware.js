359097d6e6b0334484e9a144ca2bf1f06fcd9fb2
Nitin <rajnitin793@gmail.com>
2026-07-24 03:05:37 +0530

    updates in middleware

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -16,7 +16,7 @@ async function authMiddleware(req, res, next){
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
         
         const user = await userModel.findById(decoded.userId) 
-
+ 
         
 
         req.user = user;

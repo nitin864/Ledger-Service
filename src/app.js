e58328271770b19a8c605f08022209f6c8157f27
Nitin <rajnitin793@gmail.com>
2026-07-24 02:19:37 +0530

    setuped accountRoutes in app

--- src/app.js
+++ src/app.js
@@ -4,11 +4,13 @@ config()
 const cookieParser = require('cookie-parser')
 
 const authRoutes = require('./routes/auth.routes')
+const accountRoutes = require('./routes/account.routes')
 
 const app = express()
 app.use(cookieParser())
 app.use(express.json())
 
 app.use('/api/auth', authRoutes)
+app.use('/api/account', accountRoutes)
 
 module.exports = app;

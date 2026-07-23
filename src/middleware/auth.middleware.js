8aa8430608d67f8785f188ebcd4707b73e9b8bf2
Nitin <rajnitin793@gmail.com>
2026-07-24 03:04:08 +0530

    added a middleware for JWT authentication via cookies and Bearer token

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,1 +1,34 @@
 const userModel = require('../models/user.model')
+const jwt = require('jsonwebtoken');
+
+async function authMiddleware(req, res, next){
+
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+
+    if(!token){
+        return res.status(401).json({
+            message: "Unauthorized access, please login first",
+            status: "failed"
+        })
+    }
+
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        
+        const user = await userModel.findById(decoded.userId) 
+
+        
+
+        req.user = user;
+        return next();
+
+    } catch(err){
+        console.log(err)
+        return res.status(401).json({
+            message: "Unauthorized access, please login first",
+            status: "failed"
+        })
+    }
+}
+
+module.exports = authMiddleware

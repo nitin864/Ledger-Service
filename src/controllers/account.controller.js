9b18de668b00779974e10264890171c0a611860c
Nitin <rajnitin793@gmail.com>
2026-07-25 23:10:31 +0530

    fixes

--- src/controllers/account.controller.js
+++ src/controllers/account.controller.js
@@ -2,8 +2,8 @@ const accountModel = require('../models/account.model')
 
 async function creeateAccountController(req, res) {
     try {
-        const accountData = req.body;
-        const newAccount = await accountModel.createAccount(accountData);
+        const user  = req.user;
+        const newAccount = await accountModel.createAccount({ ...req.body, userId: user.id });
         res.status(201).json(newAccount);
     } catch (error) {
         res.status(500).json({ error: 'Failed to create account' });

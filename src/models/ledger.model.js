22982176c1b37c77553e51efa73d9430bd40ee8c
Nitin <rajnitin793@gmail.com>
2026-07-28 16:03:28 +0530

    updates in leadger model

--- src/models/ledger.model.js
+++ src/models/ledger.model.js
@@ -2,5 +2,12 @@ const moongose = require('mongoose');
 
 const ledgerSchema = new mongoose.Schema({
 
-
+    account : {
+        type: moongose.Schema.Types.ObjectId,
+        ref: "accounts",
+        required : [true, "Ledger must be associated with an account"],
+        index: true,
+        immutable: true
+    }
+ 
 })

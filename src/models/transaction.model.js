a689b8654c6b062b7b1e868fb98a024d3eacb4bb
Nitin <rajnitin793@gmail.com>
2026-07-28 15:38:36 +0530

    creating a transaction model

---
+++ src/models/transaction.model.js
@@ -0,0 +1,21 @@
+const moongose = require('mongoose');
+
+const transactionSchema = new moongose.Schema({
+    fromAccount: {
+        type: moongose.Schema.Types.ObjectId,
+        ref: "accounts",
+        required: [true, "Transaction must be associated with a from account"],
+        index: true,
+    },
+    toAccount: {
+        type: moongose.Schema.Types.ObjectId,
+        ref: "accounts",
+        required: [true, "Transaction must be associated with a to account"],
+        index: true,
+    },
+    amount: {
+        type: Number,
+        required: [true, "Transaction must have an amount"],
+        min: [0, "Transaction amount must be positive"],
+    },
+})   

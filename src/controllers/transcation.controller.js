2a1b1b86edaa44d75cde9b742e857ff23ee529d0
Nitin <rajnitin793@gmail.com>
2026-07-28 17:21:17 +0530

    updates in transacction controller

--- src/controllers/transcation.controller.js
+++ src/controllers/transcation.controller.js
@@ -1,3 +1,16 @@
-async function name(params) {
-    
+const transactionModel = require('../models/transaction.model')
+const ledgerModel = require('../models/ledger.model')
+const accountModel = require('../models/account.model')
+const mongoose = require('mongoose')
+
+async function  createTransaction(){
+    const {fromAccount , toAccount, amount, idempotencyKey} = req.body;
+
+    if(!fromAccount || !toAccount || !amount || !idempotencyKey){
+        return res.status(400).json({
+            message: "fromAccount, toAccount, amount and idempotencyKey are required fields"
+          })
+    }
+
+
 }

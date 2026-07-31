8916459111272cce15f2d007c1d74e17e4bb8830
Nitin <rajnitin793@gmail.com>
2026-07-31 23:39:16 +0530

    calculate account balance from ledger entrie

--- src/models/account.model.js
+++ src/models/account.model.js
@@ -31,7 +31,35 @@ accountSchema.index({ user: 1, status: 1})
 accountSchema.methods.getBalance = async function(){
     const getBalance = await ledgerModel.aggregate([
         { $match: { account: this._id}},
+        {
+            $group:{
+                _id: null,
+                totalDebit: {
+                    $sum:{
+                        $cond:[
+                            {$eq: ["$type", "DEBIT"]},
+                            "$amount",
+                            0
+                        ]
+                    }
+                },
+                totalCredit: {
+                    $sum:{
+                        $cond:[
+                            {$eq: ["$type", "CREDIT"]},
+                            "$amount",
+                            0
+                        ]
+                    }
+                },
+                $project: {
+                    _id: 0,
+                    balance: { $subtract: ["$totalCredit", "$totalDebit"] },
+                }
+            }
+        }
     ])
+    return getBalance[0] || { totalDebit: 0, totalCredit: 0 };
 }
 
 const accountModel = mongoose.model("Account", accountSchema);
